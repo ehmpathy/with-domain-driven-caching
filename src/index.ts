d5e09eaedd17931ed969b32bac4ab839d15bdd6c
Ulad Kasach <uladkasach@gmail.com>
2023-08-07 04:01:56 -0700

    feat(wrap): add withDomainDrivenGets cache wrapper

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { ref } from './logic/ref/ref';
 export { withMutationEffects } from './logic/withMutationEffects';
 export { withQueryCaching } from './logic/withQueryCaching';
+export { withDomainDrivenGets } from './logic/withDomainDrivenGets';
 export * from './domain/DomainDrivenQueryDependency';
 export * from './domain/DomainDrivenQueryDependsOn';
---
+++ src/logic/withDomainDrivenGets.ts
@@ -0,0 +1,23 @@
+import { withoutSet } from 'simple-lambda-client';
+import {
+  withDenormalization,
+  withSerialization,
+} from 'with-cache-normalization';
+import { SerializableObject } from 'with-cache-normalization/dist/domain/NormalizeCacheValueMethod';
+import { SimpleCache } from 'with-simple-caching';
+
+/**
+ * a method which makes it easy to get from a domain-driven cache, without access to the query logic itself
+ *
+ * specifically
+ * - adds withoutSet to the cache, since only the withQueryCaching wrapped method should be calling that
+ * - adds withSerialization to the cache, to make it compatible with normalization
+ * - adds withDenormalization to the cache, to enable it to denormalize the normalized cached query outputs
+ *
+ * usecase
+ * - accessing outputs .set by a lambda server, at the lambda client (e.g., with [simple-lambda-client](https://github.com/ehmpathy/simple-lambda-client))
+ */
+export const withDomainDrivenGets = (
+  cache: SimpleCache<string>,
+): SimpleCache<SerializableObject> =>
+  withoutSet(withDenormalization(withSerialization<SerializableObject>(cache)));
